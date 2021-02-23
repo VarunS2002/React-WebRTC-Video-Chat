69bc50d7b46604a2e1edf5528075eeda3ae3135c
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2021-02-23 15:29:16 +0530

    - Documented VideoChat.js - Split onEndCallClicked to a separate function

--- src/video_chat/VideoChat.js
+++ src/video_chat/VideoChat.js
@@ -7,12 +7,21 @@ export default class VideoChat extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            /** @type {boolean} */
             isLoggedIn: false,
+            /** @type {string} */
             userToCall: '',
+            /** @type {string} */
             username: ''
         }
     }
 
+    /**
+     * It calls the onLogin function and passes the required parameter.
+     * It is called when the Login button is clicked.
+     *
+     * @return {Promise<void>}
+     */
     onLoginClicked = async () => {
         await this.props.onLogin(this.state.username)
         this.setState({
@@ -20,12 +29,39 @@ export default class VideoChat extends React.Component {
         })
     }
 
+    /**
+     * It calls the startCall function and passes the required parameters.
+     * It is called when the Call button is clicked.
+     *
+     * @return {void}
+     */
     onStartCallClicked = () => {
         this.props.startCall(this.state.username, this.state.userToCall)
     }
 
+    /**
+     * It calls the endCall function.
+     * It is called when the End Call button is clicked.
+     *
+     * @return {void}
+     */
+    onEndCallClicked = () => {
+        this.setState({
+            youDisconnected: true
+        })
+        endCall()
+    }
+
+    /**
+     * Displays the video streams in frames.
+     * It is called when you log in and displays your own video stream.
+     * It displays the video streams of both the users when a call is active.
+     *
+     * @return {JSX.Element}
+     */
     renderVideos() {
         return <div className={this.state.isLoggedIn ? 'videos active' : 'videos'}>
+            {/*Displays your video stream*/}
             <div>
                 <label>{this.state.username}</label>
                 <video ref={this.props.setLocalVideoRef} autoPlay playsInline muted="muted"/>
@@ -34,6 +70,7 @@ export default class VideoChat extends React.Component {
                     <video ref={this.props.setLocalVideoRef} autoPlay playsInline muted="muted"/> :
                     <video ref={this.props.setLocalVideoRef} autoPlay playsInline/>}*/}
             </div>
+            {/*Displays the other person's video stream*/}
             <div>
                 <label>{this.props.connectedUser}</label>
                 <video ref={this.props.setRemoteVideoRef} autoPlay playsInline/>
@@ -41,18 +78,25 @@ export default class VideoChat extends React.Component {
         </div>
     }
 
+    /**
+     * Creates the Option buttons.
+     * It is called if the call is active.
+     *
+     * @return {JSX.Element}
+     */
     renderOptions() {
         return <div className="options">
-            <button onClick={() => {
-                this.setState({
-                    youDisconnected: true
-                })
-                endCall()
-            }} id="end-call-btn" className="btn btn-primary">End Call
-            </button>
+            <button onClick={this.onEndCallClicked} id="end-call-btn" className="btn btn-primary">End Call</button>
         </div>
     }
 
+    /**
+     * Binds Enter key to click Login or click Call on their respective pages.
+     *
+     * @param {KeyboardEvent} event
+     *
+     * @return {void}
+     */
     bindEnterKey(event) {
         if (event.key === "Enter" && document.activeElement.id === "login-input") {
             document.getElementById("login-btn").click()
@@ -65,10 +109,17 @@ export default class VideoChat extends React.Component {
         }
     }
 
+    /**
+     * Creates the Login Form.
+     * It is the first page and remains active until a call starts.
+     *
+     * @return {JSX.Element}
+     */
     renderForms() {
         window.addEventListener('keypress', this.bindEnterKey)
 
         return this.state.isLoggedIn ?
+            // Rendered if user has not logged in
             <div key='a' className='form'>
                 <label>Enter contact name to call:</label>
                 <input value={this.state.userToCall} type="text" id="contact-input" autoFocus
@@ -76,18 +127,29 @@ export default class VideoChat extends React.Component {
                 <button onClick={this.onStartCallClicked} id="call-btn" className="btn btn-primary">Call</button>
             </div>
             :
+            // Rendered if user has logged in
             <div key='b' className='form'>
-                <label>Enter your name:</label>
+                <label>Enter your username:</label>
                 <input value={this.state.username} type="text" id="login-input" autoFocus
                        onChange={e => this.setState({username: e.target.value})}/>
                 <button onClick={this.onLoginClicked} id="login-btn" className="btn btn-primary">Login</button>
             </div>
     }
 
+    /**
+     * Renders all the Elements including the Login form, Video frames and the Options.
+     * Chooses what to render based on certain conditions.
+     * This is the entry point of the website.
+     *
+     * @return {JSX.Element}
+     */
     render() {
         return <section id="container" style={{marginTop: "-25px"}}>
+            {/*Renders Login form if a call is not active*/}
             {this.props.connectedUser ? null : this.renderForms()}
+            {/*Renders the video streams*/}
             {this.renderVideos()}
+            {/*Renders the Option buttons if a call is active*/}
             {this.props.connectedUser ? this.renderOptions() : null}
         </section>
     }
